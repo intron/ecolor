f822178ce978bdb4b4121de2573dbd07048ff5eb
Alex Alekseyenko <alex.alekseyenko@gmail.com>
2014-07-11 22:30:47 -0700

    clean up ecolor.js

--- client/ecolor.js
+++ client/ecolor.js
@@ -1,30 +1,72 @@
 if (Meteor.isClient) {
   var clientStartTime = Date.now();
 
-
-
   Template.hello.greeting = function () {
     return "Welcome to ecolor.";
   };
 
   Template.hello.events({
+    
+    // test button creates a record in Experiments
     'click input': function () {
       var guid = Experiments.insert({ "dishID": "123abc",
                            "scanTime": Date.now()
       });
-      /*Experiments.find(guid).observe({ changed: function(updated, old) {
-
-      }});*/
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("should have created an experiment record");
     }
   });
 
+  // retrieve records in Experiments created after this session started
   Template.experimentDiv.experiments = function () {
     var cursor = Experiments.find({ scanTime: { $gt: clientStartTime} });
     return cursor;
   };
+
+  Template.visualizations.rendered = function() {
+
+    var chart = d3.select("#viz")
+
+    Deps.autorun(function () {
+      var coloniesData = Colonies.find().fetch();
+      var bins = Array.apply(null, new Array(255)).map(Number.prototype.valueOf,0);
+
+      coloniesData.forEach(function(colony) { 
+        bins[colony.Hue]++
+      });
+
+      console.log(bins);
+
+      chart.selectAll("div")
+        .remove();
+
+      chart.selectAll("div")
+        .data(bins)
+        .enter()
+        .append("div")
+        .style("background-color", function(d, i) { 
+          return hslaify({ Hue: i, Saturation: 100});})
+        .style("width", function (d, i) { 
+          console.log( d + " " + i); 
+          return d + "px";})
+        .style("height", function() { return "4px" })
+    });
+  };
+
+////////////
+// HELPERS
+////////////
+
+  // creates hsla-style string from colony record
+  function hslaify(d) {
+    return "hsla(" + d.Hue + "," + d.Saturation + "%,50%,1)";  
+  }
+
+}
+
+
+
   
 
 /*
@@ -75,45 +117,3 @@ scaling bars by area
 
   */
 
-  Template.visualizations.rendered = function() {
-    // use if we only want recently-added records
-    // var query = { scanTime: { $gt: clientStartTime}, rgb: { $exists: true} };
-
-    var chart = d3.select("#viz")
-
-    Deps.autorun(function () {
-      var coloniesData = Colonies.find().fetch();
-      var bins = Array.apply(null, new Array(255)).map(Number.prototype.valueOf,0);
-
-      coloniesData.forEach(function(colony) { 
-        bins[colony.Hue]++
-      });
-
-      console.log(bins);
-
-      chart.selectAll("div")
-        .remove();
-
-      chart.selectAll("div")
-        .data(bins)
-        .enter()
-        .append("div")
-        .style("background-color", function(d, i) { 
-          return hslaify({ Hue: i, Saturation: 100});})
-        .style("width", function (d, i) { 
-          console.log( d + " " + i); 
-          return d + "px";})
-        .style("height", function() { return "4px" })
-    });
-  };
-
-////////////
-// HELPERS
-////////////
-
-  // creates hsla style string from colony record
-  function hslaify(d) {
-    return "hsla(" + d.Hue + "," + d.Saturation + "%,50%,1)";  
-  }
-
-}
