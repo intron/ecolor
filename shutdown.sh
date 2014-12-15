a9faaaafa106d9005696f0cc9773399a2890b458
fgcapo <fgcapo@gmail.com>
2014-12-15 14:58:53 -0800

    changed startup scripts to use home folder

--- shutdown.sh
+++ shutdown.sh
@@ -1,1 +1,2 @@
-/Users/VizServer/.meteor/packages/meteor-tool/1.0.34/meteor-tool-os.osx.x86_64/dev_bundle/mongodb/bin/mongo --port 3001 --eval "db.getSiblingDB('admin').shutdownServer()"
+~/.meteor/packages/meteor-tool/.1.0.30.gpevpe++os.linux.x86_64+web.browser+web.cordova/meteor-tool-os.linux.x86_64/dev_bundle/mongodb/bin/mongo --port 3001 --eval "db.getSiblingDB('admin').shutdownServer()"
+
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-o#local ip address: 10.4.98.52
+#local ip address: 10.4.98.52
 
 export MONGO_URL="mongodb://127.0.0.1:3001/meteor"
 
 #run as daemon (using —fork)
 #--dbpath /code/ecolor/.meteor/local/db
-/Users/VizServer/.meteor/packages/meteor-tool/1.0.34/meteor-tool-os.osx.x86_64/dev_bundle/mongodb/bin/mongod --fork --bind_ip 0.0.0.0 --smallfiles --nohttpinterface --port 3001 --logpath /code/db/log/mongodb.log --dbpath /code/db
+~/.meteor/packages/meteor-tool/.1.0.30.gpevpe++os.linux.x86_64+web.browser+web.cordova/meteor-tool-os.linux.x86_64/dev_bundle/mongodb/bin/mongod --fork --bind_ip 0.0.0.0 --smallfiles --nohttpinterface --port 3001 --logpath /code/db/log/mongodb.log --dbpath /code/db
 
 meteor
